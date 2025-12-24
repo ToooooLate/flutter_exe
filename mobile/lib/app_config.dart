13d56f50a7b0ff46d47b270656241a2505c17bf2
harry <harry.sun@sangoai.cn>
2025-12-24 16:55:02 +0800

    fix: 更新Jitsi服务器URL和主机地址

--- mobile/lib/app_config.dart
+++ mobile/lib/app_config.dart
@@ -1,5 +1,4 @@
 class AppConfig {
-  static const jitsiServerUrl = 'https://qingzhi.sangoai.com:9443';
-  static const jitsiHost = 'qingzhi.sangoai.com';
+  static const jitsiServerUrl = 'https://qz-wh.sangoai.com:9443';
+  static const jitsiHost = 'qz-wh.sangoai.com';
 }
-
